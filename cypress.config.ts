892bcfc51bd97de7c9e59c0cb5fc7f9b069e0811
arshak1988 <arshakpaskevichyan@gmail.com>
2026-04-03 11:21:31 +0400

    commit N002 Update baseUrl to vite preview port

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5174/",
+    baseUrl: process.env.CI ? "http://localhost:4173/" : "http://localhost:5173/",
     specPattern: "cypress/e2e/**/*.cy.ts",
-    supportFile: "cypress/support/e2e.ts"
+    supportFile: "cypress/support/e2e.ts",
+    allowCypressEnv: false
   }
 });
-  
